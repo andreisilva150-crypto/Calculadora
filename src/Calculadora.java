62f22a92cb924b463dbcbbf31aa96b59cd6124b2
Andrei da Silva <andrei_silva150@estudnte.sesisenai.org.br>
2026-07-16 22:09:36 -0300

    Calculadora completa

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,36 @@
+
+import java.util.Scanner;
+
 public class Calculadora {
-    static void main(String[] args) {
-        System.out.println("Segundo repósitório");
+    static void soma (int n1, int n2) {
+        System.out.println("A Soma é: " + n1 + n2);
+            }
+    static void subtracao (int n1, int n2) {
+        System.out.println("A subtracao é: " + (n1 - n2));
+        }
+    static void multiplicacao (int n1, int n2) {
+        System.out.println("A Soma é: " + (n1 * n2));
+    }
+    static void divisao (int n1, int n2) {
+        System.out.println("A Soma é: " + (n1 / n2));
+        if (n2==0){
+            System.out.println("Não é possível por zero");
+        }else{
+            System.out.println("A divisão é " + (n1/n2));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner leia = new Scanner (System.in);
+        System.out.println("Digite o primeiro número:");
+        int n1 = leia.nextInt();
+        System.out.println("Digite o segundo número:");
+        int n2 = leia.nextInt();
+
+        soma(n1,n2);
+        subtracao(n1,n2);
+        multiplicacao(n1,n2);
+        divisao(n1,n2);
     }
 }
+
+
